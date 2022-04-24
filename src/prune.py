d6266a37f234339f1f888c8a3ab5bfb23d1fa7a8
Benjamin Bock <benji.b2214@gmail.com>
2022-04-24 20:53:03 +0000

    redid formating, added prune test loop

--- src/prune.py
+++ src/prune.py
@@ -67,7 +67,21 @@ def sparsity_calculation(model):
     print(f"Non-Zero Parameters: {total_nonzeros}")
     print(f"Total Parameters: {total_param}")
     print(f"Sparsity: {float(total_zeros/total_param)}")
+def test_model(model,test_loader):
+    with torch.no_grad():
+        test_correct = 0
+        test_total = 0
+        for i, data in enumerate(test_loader):
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
 
+            outputs = model(images)
+            # ouputs_original = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            test_total += labels.size(0)
+            test_correct += (predicted == labels).sum().item()
+    return 100 * test_correct / test_total, test_total
+    # print(f'Accuracy of the network on the {test_total} test images: {100 * test_correct / test_total} %')
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -85,47 +99,18 @@ if __name__ == '__main__':
                                               drop_last = True,
                                               shuffle = True
     )
-
-    test_correct = 0
-    test_total = 0
-
-    with torch.no_grad():
-        for i, data in enumerate(test_loader):
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            test_total += labels.size(0)
-            test_correct += (predicted == labels).sum().item()
-
-    print(f'Accuracy of the network on the {test_total} test images: {100 * test_correct / test_total} %')
-    new_model = copy.deepcopy(model)
+    new_model = mobilevit_xxs()
+    
 
     sparsity_calculation(new_model)
 
     # new_model = prune_model_l1_unstructured(new_model)
-    new_model = prune_model_global_structured(new_model,.65)
-    
-    torch.save(model.state_dict(), "./saved_models/pruned_weights.pth")
-
-    print("Testing Pruned")
-
-
-
-    
-    test_correct = 0
-    test_total = 0
-
-    with torch.no_grad():
-        for i, data in enumerate(test_loader):
-            images, labels = data
-            images, labels = images.to(device), labels.to(device)
-
-            outputs = new_model(images)
-            ouputs_original = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            test_total += labels.size(0)
-            test_correct += (predicted == labels).sum().item()
-
-    print(f'Accuracy of the network on the {test_total} test images: {100 * test_correct / test_total} %')
+    for i in range(20):
+        new_model.load_state_dict(torch.load("./saved_models/unpruned_weights.pth"))
+        new_model = new_model.to(device)
+        new_model = prune_model_global_structured(new_model,i*.05)
+        sparsity_calculation(new_model)
+        torch.save(model.state_dict(), f"./saved_models/pruned_weights{i*5}%.pth")
+        print(f'\nGlobal Pruning with {i*5}% weights removed')
+        acc,test_total = test_model(new_model,test_loader)
+        print(f'Accuracy of the network on the {test_total} test images: {acc}%') 
